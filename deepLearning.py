2f992d139e595d9adb3c986080390980ea067132
Ayşe YALÇINKAYA <63245996+ayseyk@users.noreply.github.com>
2022-07-02 12:49:48 +0300

    Update deepLearning.py

--- deepLearning.py
+++ deepLearning.py
@@ -4,54 +4,52 @@ Created on Fri Dec 17 15:26:01 2021
 
 @author: YALÇINKAYA
 """
-#import tensorflow as tf
 
-from keras.applications.mobilenet import MobileNet, preprocess_input
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
-from keras.optimizers import RMSprop
-from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+import tensorflow as tf
 
+from tensorflow.keras.models import Sequential          # ardışıl CNN modelleri oluşturmak için
+from tensorflow.keras.layers import Flatten, Dense      # CNN katmanları için
+from tensorflow.keras.optimizers import RMSprop,Adam    # öğrenme algoratması
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-from matplotlib import pyplot as plt
-import numpy as np
+import keras
 
+img_dim = (224,224,3)
+ind = 4
+vgg16_model, inception_model, resnet_model, xception_model, mobilenet_model = Sequential(),Sequential(),Sequential(),Sequential(),Sequential()
+if ind==0:
+    vgg16_model = tf.keras.applications.vgg16.VGG16(weights='imagenet', include_top=False, input_shape=img_dim)
+elif ind==1:
+    inception_model = tf.keras.applications.inception_v3.InceptionV3(weights='imagenet', include_top=False, input_shape=img_dim)
+elif ind==2:    
+    resnet_model = tf.keras.applications.resnet50.ResNet50(weights='imagenet', include_top=False, input_shape=img_dim)
+elif ind==3:    
+    xception_model = tf.keras.applications.xception.Xception(weights='imagenet', include_top=False, input_shape=img_dim)
+else:    
+    mobilenet_model = tf.keras.applications.mobilenet.MobileNet(weights='imagenet', include_top=False, input_shape=img_dim)
 
+model_array = [vgg16_model, inception_model, resnet_model, xception_model, mobilenet_model]
+model_str = ["vgg16_model", "inception_model", "resnet_model", "xception_model", "mobilenet_model"]
+conv_model = model_array[ind]
 
-
-conv_model = MobileNet(weights='imagenet', 
-                       include_top=False,
-                       input_shape=(224, 224, 3))
-
+conv_model.trainable = False
 conv_model.summary()
-
-conv_model.trainable = True
-set_layer_trainable = False
-
-for layer in conv_model.layers:
-    if layer.name == 'conv_dw_13':# 'conv_dw_13'ten itibaren eğitir // block5_conv1
-        set_layer_trainable = True
-    if set_layer_trainable:
-        layer.trainable = True
-    else:
-        layer.trainable = False
-
-
 
 model = Sequential()
 
 model.add(conv_model)
 model.add(Flatten())
 model.add(Dense(256, activation='relu')) #512,256
-model.add(Dense(6, activation='softmax'))
+model.add(Dense(256, activation='relu')) #512,256
+model.add(Dense(16, activation='softmax'))
+
 
 
 model.compile(loss='binary_crossentropy', #categorical_crossentropy,binary_crossentropy
-              optimizer=RMSprop(lr=1e-5),
+              optimizer=Adam(learning_rate=1e-3), # lr=0.001
               metrics=['acc']) #categorical_accuracy, acc
 
 model.summary()
-
 
 train_data = 'dataset/train'
 validation_data = 'dataset/validation'
@@ -72,7 +70,7 @@ train_datagen = ImageDataGenerator(
 train_generator = train_datagen.flow_from_directory(
         train_data,
         target_size=(224, 224),
-        batch_size=32,
+        batch_size=16,
         )
 
 
@@ -83,16 +81,14 @@ validation_datagen = ImageDataGenerator(
 validation_generator = validation_datagen.flow_from_directory(
         validation_data,
         target_size=(224, 224),
-        batch_size=32,
+        batch_size=16,
         )
 
 
 history = model.fit(
           train_generator,
-          steps_per_epoch=28,
-          epochs=10, #sadece epoch olabilir.
           validation_data=validation_generator,
-          validation_steps=2
+          epochs=50, 
           )
 
 model.save('classification_model.h5')
@@ -109,21 +105,5 @@ test_generator = test_datagen.flow_from_directory(
         )
 
 
-results = model.evaluate(test_generator,steps=8) # step silinebilir.
+results = model.evaluate(test_generator) 
 print("test loss, test acc:", results)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
